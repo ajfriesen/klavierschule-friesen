7ca2a5460da564702d4cbddc3ab171983598eee9
ajfriesen <andre.friesen@gmail.com>
2017-12-31 14:49:44 +0100

    Initial setup

--- keyboard_shool_project/wsgi.py
+++ keyboard_shool_project/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for klavierschule_friesen project.
+WSGI config for keyboard_shool_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "klavierschule_friesen.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "keyboard_shool_project.settings")
 
 application = get_wsgi_application()
